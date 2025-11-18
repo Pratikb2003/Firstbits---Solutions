c47103c2ad11882307b83927850745fee0ef5cf8
Pratikb2003 <pratikborkar7071@gmail.com>
2025-11-18 19:34:02 +0530

    first commit

---
+++ Ba/HelloWorld.java
@@ -0,0 +1,11 @@
+package Ba;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
+
